2db181072099d2df322c43c5a1383080dc43ee57
nikhilkumbhare <nikhilk190697@gmail.com>
2021-03-21 00:31:29 +0530

    Added: Given State Census File is Incorrect Should Throw Exception

--- src/test/java/CensusAnalyserTest.java
+++ src/test/java/CensusAnalyserTest.java
@@ -4,16 +4,27 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CensusAnalyserTest {
+
     public static final String INDIAN_STATE_CENSUS_FILE = "C:\\Users\\nikhi\\IdeaProjects\\CensusAnalyserProblem\\src\\main\\resources\\IndiaStateCensusData.csv";
+    public static final String WRONG_CSV_FILE = "C:\\Users\\nikhi\\IdeaProjects\\CensusAnalyserProblem\\src\\main\\resources\\IndiaStateCode";
 
 
     @Test
-    public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() {
+    public void  givenIndianStateCensusCSVFile_ReturnsCorrectRecords() {
         try {
             int count = CensusAnalyser.loadCensusData(INDIAN_STATE_CENSUS_FILE);
             Assert.assertEquals(29, count);
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenIndiaStateCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser.loadCensusData(WRONG_CSV_FILE);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
         }
     }
 }
